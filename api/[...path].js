306004ca004b2c7baaeea24488b51aaacff55818
ivandertheo <ivander.suryono2@gmail.com>
2026-05-02 20:39:46 +0700

    fix vercel api build

--- api/[...path].js
+++ api/[...path].js
@@ -1,8 +1,8 @@
 export default async function handler(req, res) {
   const BASE_URL = "http://38.47.180.195/student08/api";
 
-  const path = req.query.path?.join("/") || "";
-  const targetUrl = `${BASE_URL}/${path}`;
+  const path = req.url.replace("/api", "");
+  const targetUrl = BASE_URL + path;
 
   try {
     const response = await fetch(targetUrl, {
@@ -17,16 +17,16 @@ export default async function handler(req, res) {
           : undefined,
     });
 
-    const text = await response.text();
+    const data = await response.text();
 
     res.status(response.status);
 
     try {
-      res.json(JSON.parse(text));
+      res.json(JSON.parse(data));
     } catch {
-      res.send(text);
+      res.send(data);
     }
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 }
